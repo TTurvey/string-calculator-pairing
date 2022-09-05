a531710f75ebdf97706ffb8eba46a133a9b29783
Tim Turvey <tturvey@hotmail.co.uk>
2022-09-05 14:28:02 +0100

    Green: 3 numbers passes

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,9 +1,23 @@
 public class StringCalculator {
 
-
-
         int add(String numbers) {
-                return 0;
+                if (numbers.equals("")) {
+                        return 0;
+                }
+
+                String[] numbersArray = numbers.split(",");
+
+                if (numbersArray.length == 1) {
+                        return Integer.parseInt(numbers);
+                }
+                if (numbersArray.length == 2) {
+                        return Integer.parseInt(numbersArray[0]) + Integer.parseInt(numbersArray[1]);
+                }
+                if (numbersArray.length == 3) {
+                        return Integer.parseInt(numbersArray[0]) + Integer.parseInt(numbersArray[1]) + Integer.parseInt(numbersArray[2]);
+                } else {
+                        return -1;
+                }
         }
 
 }
--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -1,13 +1,37 @@
 import org.testng.annotations.Test;
 
-import static org.testng.Assert.*;
+import static org.testng.Assert.assertEquals;
 
 public class StringCalculatorTest {
 
 
-
     @Test
     void returns_0_with_empty_string() {
+
         assertEquals(0, new StringCalculator().add(""));
     }
+
+    @Test
+    void returns_number_with_one_number() {
+
+        assertEquals(1, new StringCalculator().add("1"));
+    }
+
+    @Test
+    void returns_sum_with_two_numbers() {
+        assertEquals(2, new StringCalculator().add("1,1"));
+    }
+    @Test
+    void returns_15_with_10_and_5() {
+        assertEquals(15, new StringCalculator().add("10,5"));
+    }
+
+    @Test
+    void returns_sum_with_arbitrary_number_size() {
+        assertEquals(6, new StringCalculator().add("1,2,3"));
+    }
+
+
+
+
 }
