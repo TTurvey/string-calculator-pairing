3979332eb3e87fb4d81e5b74c6d8bd883596f34f
Tim Turvey <tturvey@hotmail.co.uk>
2022-09-05 15:58:55 +0100

    Red: custom seperator fails

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,23 +1,29 @@
 public class StringCalculator {
 
         int add(String numbers) {
-                if (numbers.equals("")) {
+                if (numbers.equals("")) { return 0; }
+                if (numbers.length() == 1) { return Integer.parseInt(numbers); }
+
+                char separator = 'x';
+                if (numbers.substring(0,2).equals("//") ) {
+                        separator = numbers.charAt(2);
+                }
+
+
+                String[] numbersArray = numbers.split(",\n|" + separator);
+
+                if (numbersArray.length > 1) {
+                        int sum = 0;
+                        for (int i = 0; i < numbersArray.length; i++) {
+                                sum += Integer.parseInt(numbersArray[i]);
+                        }
+                        return sum;
+                } else {
                         return 0;
                 }
-
-                String[] numbersArray = numbers.split(",");
-
-                if (numbersArray.length == 1) {
-                        return Integer.parseInt(numbers);
-                }
-                if (numbersArray.length == 2) {
-                        return Integer.parseInt(numbersArray[0]) + Integer.parseInt(numbersArray[1]);
-                }
-                if (numbersArray.length == 3) {
-                        return Integer.parseInt(numbersArray[0]) + Integer.parseInt(numbersArray[1]) + Integer.parseInt(numbersArray[2]);
-                } else {
-                        return -1;
-                }
         }
-
 }
+
+//for (String str : myStringArray) {
+//        sum += Integer.parseInt(number);
+//}
--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -27,8 +27,20 @@ public class StringCalculatorTest {
     }
 
     @Test
-    void returns_sum_with_arbitrary_number_size() {
+    void returns_sum_with_3_numbers() {
         assertEquals(6, new StringCalculator().add("1,2,3"));
+    }
+    @Test
+    void returns_sum_with_arbitrary_number_size() {
+        assertEquals(45, new StringCalculator().add("1,2,3,4,5,6,7,8,9"));
+    }
+    @Test
+    void returns_sum_with_new_line_separator() {
+        assertEquals(6, new StringCalculator().add("1\n2,3"));
+    }
+    @Test
+    void returns_sum_with_custom_separator() {
+        assertEquals(3, new StringCalculator().add("//;\n1;2"));
     }
 
 
